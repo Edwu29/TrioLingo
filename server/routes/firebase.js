eedc779e29a68548667d4b9ea1c16fddee4502d5
Aaron Chen Laptop <chenaaron3@gmail.com>
2021-01-30 21:31:39 -0800

    language integration

--- server/routes/firebase.js
+++ server/routes/firebase.js
@@ -9,7 +9,6 @@ router.get("/initialize_database", async function (req, res, next) {
 });
 
 router.put("/append_oneObject", async function (req, res, next) {
-
     var key = req.body.key;
     appendObject(key, {
         photo: "base64:asdgdsed",
